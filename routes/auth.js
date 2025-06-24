204dfbbbc81db635fa45a8d68c43f1c5da17ef28
Harshana Bhandari <np03cs4a230186@heraldcollege.edu.np>
2025-06-24 14:54:19 +0545

    refactor: the login endpoint was refactored using database

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { signup,login  } = require('../controllers/authcontrollers');
+const { login  } = require('../controllers/authcontrollers');
 
-router.post('/signup', signup);
+// router.post('/signup', signup);
 router.post('/login', login);
 
 
